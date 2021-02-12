888a2b1040eaf274380b5267cd6acf2822c06f11
VvWijhe <victor.v.wijhe@hotmail.com>
2021-02-12 18:41:57 +0100

    finnished compare

--- test_env_golden_unit.vhd
+++ test_env_golden_unit.vhd
@@ -1,6 +1,5 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use work.pattern_recognizer.all;
 
 
 entity test_env_golden_unit is
@@ -8,6 +7,18 @@ end test_env_golden_unit;
 
 
 architecture test of test_env_golden_unit is
+
+	component pattern_recognizer is 
+		port(	clk 	: in std_logic;
+			reset : in std_logic;
+			data 	: in std_logic;
+			seg1	: out std_logic_vector(6 DOWNTO 0);
+			seg2	: out std_logic_vector(6 DOWNTO 0)	
+		);
+	end component;
+
+
+
 begin
 
 
